9ccbd24cb11fcadefa3217221d672a064c0f10bd
Monica Singh <monicas138@gmail.com>
2015-09-10 12:14:25 +0530

    refactored and added spec  for non exempted but imported

--- src/com/tw/baseline/TotalSalesTax.java
+++ src/com/tw/baseline/TotalSalesTax.java
@@ -5,14 +5,20 @@ public class TotalSalesTax {
 
     private String itemName;
     private double itemPrice;
+    private  int noOfItems;
 
-    public TotalSalesTax(String itemName, double itemPrice) {
+
+    public TotalSalesTax(int noOfItems, String itemName, double itemPrice) {
         this.itemName = itemName;
         this.itemPrice = itemPrice;
+        this.noOfItems = noOfItems;
+
     }
 
     public double calculateTotalTax() {
         BasicTax basicTax = new BasicTax(itemName, itemPrice);
-        return (basicTax.calculateBasicTax());
+        AdditionalTax additionalTax = new AdditionalTax(itemName, itemPrice);
+        return (basicTax.calculateBasicTax() + additionalTax.calculateAdditionalTax());
     }
+
 }
--- test/com/tw/baseline/TotalSalesTaxTest.java
+++ test/com/tw/baseline/TotalSalesTaxTest.java
@@ -8,23 +8,30 @@ public class TotalSalesTaxTest {
 
     @Test
     public void shouldReturnTotalTaxAsZeroIfNoItemIsPurchased() {
-        TotalSalesTax totalSalesTax = new TotalSalesTax("item 1", 0.0);
+        TotalSalesTax totalSalesTax = new TotalSalesTax(1, "item 1", 0.0);
 
         assertEquals(0.0, totalSalesTax.calculateTotalTax(), 0.0d);
     }
 
     @Test
     public void shouldReturnTotalTaxForItemWhichIsExemptedAndNotImported() {
-        TotalSalesTax totalSalesTax = new TotalSalesTax("i am exempted and not from outside so i am totally tax free", 10.0);
+        TotalSalesTax totalSalesTax = new TotalSalesTax(1, "i am exempted and not from outside so i am totally tax free", 10.0);
 
-        assertEquals(0.0, totalSalesTax.calculateTotalTax(), 0.0);
+        assertEquals(1.0, totalSalesTax.calculateTotalTax(), 0.0);
     }
 
     @Test
-    public void shouldCalculateTotalTaxForItemWhichIsNonImportedButNonExempted() {
-        TotalSalesTax totalSalesTax = new TotalSalesTax("any item", 100.0);
+    public void shouldCalculateTotalTaxForItemWhichIsNonImportedAndNonExempted() {
+        TotalSalesTax totalSalesTax = new TotalSalesTax(1, "any item", 100.0);
 
         assertEquals(10.0, totalSalesTax.calculateTotalTax(), 0.0000);
+    }
+
+    @Test
+    public void shouldCalculateTotalTaxForItemWhichIsNotExemptedButImported() {
+        TotalSalesTax totalSalesTax = new TotalSalesTax(1, "any imported good", 100.0);
+
+        assertEquals(60.0, totalSalesTax.calculateTotalTax(), 0.0000);
     }
 
 }
